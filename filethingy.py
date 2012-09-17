77d80e08a321762c6192da1ee7986098854e33ec
Jay Skally <@J_Skally>
2012-09-16 22:24:46 -0500

    gaza increment 1

---
+++ filethingy.py
@@ -0,0 +1,18 @@
+#cuz im so gaza imma make ur files better
+#real basic..this is the skeleton code doe pree
+
+#works with large files..it prees dem in chunks so doe sweat if it takes a while
+
+import hashlib
+
+def md5Checksum(filePath):
+    fh = open(filePath, 'rb')
+    m = hashlib.md5()
+    while True:
+        data = fh.read(8192)
+        if not data:
+            break
+        m.update(data)
+    return m.hexdigest()
+
+print 'The MD5 checksum of text.txt is', md5Checksum('test.txt')
